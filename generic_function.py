8d6bc43016271571c32263626c5f86c73f26f49e
ffayad <ffayad@ptcdb.edu.ps>
2025-01-11 14:05:24 +0200

    return multiple results

--- generic_function.py
+++ generic_function.py
@@ -4,3 +4,13 @@ def genral_function(*x):
         sum+=i
     return sum
 print(genral_function(1, 8, 5, 9))
+
+def sum_min_max(*x):
+    sum = 0
+    for i in x:
+        sum+=i
+    ma = max(x)
+    mi = min(x)
+    return sum, ma, mi
+
+print(sum_min_max(10, 8, 5, 9))
