5260d37ef9eb8f184694d2d19611417074ef5fab
ffayad <ffayad@ptcdb.edu.ps>
2025-01-12 12:45:35 +0200

    catch raised exception

--- factorial_function.py
+++ factorial_function.py
@@ -1,4 +1,7 @@
 #4.	Write a Python function to calculate the factorial of a number (a non-negative integer). The function accepts the number as an argument
+from warnings import catch_warnings
+
+
 def factorial(n):
     if n < 0:
         raise ValueError("Factorial is not defined for negative numbers.")
@@ -9,5 +12,8 @@ def factorial(n):
 
     return result
 
-number = 5
-print(f"The factorial of {number} is: {factorial(number)}")
+number = -5
+try:
+    factorial(number)
+except ValueError as e:
+    print(e)
